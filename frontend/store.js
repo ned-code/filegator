5fd46751aeb0fce893c0efe66f8fb5911baed5e0
bahymali <bahy@ess.mx>
2020-06-17 00:37:17 +0200

    Fix HOME PATH in production & add homedir to state

--- frontend/store.js
+++ frontend/store.js
@@ -13,7 +13,8 @@ export default new Vuex.Store({
       role: 'guest',
       permissions: [],
       name: '',
-      username: ''
+      username: '',
+      homedir:''
     },
     cwd: {
       location: '/',
