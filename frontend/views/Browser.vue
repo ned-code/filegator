56a40c6f9be4e96c92bdfcf38835c238b0cf1b30
bahymali <bahy@ess.mx>
2020-06-24 10:45:55 +0200

    Hide Copy link for Autologin

--- frontend/views/Browser.vue
+++ frontend/views/Browser.vue
@@ -143,7 +143,7 @@
                 <b-dropdown-item v-if="can('write')" aria-role="listitem" @click="remove($event, props.row)">
                   <b-icon icon="trash-alt" size="is-small" /> {{ lang('Delete') }}
                 </b-dropdown-item>
-                <b-dropdown-item v-if="props.row.type == 'file' && can('download')" v-clipboard:copy="getDownloadLink(props.row.path)" aria-role="listitem">
+                <b-dropdown-item v-if="props.row.type == 'file' && can('download') && !can('autologin')" v-clipboard:copy="getDownloadLink(props.row.path)" aria-role="listitem">
                   <b-icon icon="clipboard" size="is-small" /> {{ lang('Copy link') }}
                 </b-dropdown-item>
                 <b-dropdown-item v-if="props.row.type == 'file' && can('download') && !is('guest')" v-clipboard:copy="getDirectLink(props.row.path)" aria-role="listitem">
